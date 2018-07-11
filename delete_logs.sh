79847c53bcdaaf639a676c1cac3ca0dea13f6e56
Raman Deep <deep.raman85@gmail.com>
2018-07-11 12:37:26 +0200

    Added check log dir function outside delete_logs for better code

--- delete_logs.sh
+++ delete_logs.sh
@@ -22,13 +22,16 @@ LOG_DIR="/var/www/vhosts/LOGS/s/log/debug/"
 LOG_FILE="/var/log/${DATE}_delete_logs.log"
 LOG_DAYS="4"
 
-delete_logs() {
-  echo -e "" >> "$LOG_FILE"
-  echo -e "$TIMESTAMP Checking debug log directories to delete older than $LOG_DAYS days..." >> "$LOG_FILE"
+check_log_dir() {
   if [[ ! -d "$LOG_DIR" ]]; then
     echo -e "$TIMESTAMP Log directory not found : $LOG_DIR"
     exit 1
   fi
+}
+
+delete_logs() {
+  echo -e "" >> "$LOG_FILE"
+  echo -e "$TIMESTAMP Checking debug log directories to delete older than $LOG_DAYS days..." >> "$LOG_FILE" 
   LOG_FILES=$(/bin/find "$LOG_DIR" -name "????-??-??" -mtime +$LOG_DAYS)
   if [[ -z "$LOG_FILES" ]]; then
     echo "$TIMESTAMP  No log directories found older than : $LOG_DAYS day/s." >> "$LOG_FILE"
@@ -41,5 +44,8 @@ delete_logs() {
   fi
 }
 
+check_log_dir
 delete_logs
+
 exit 0
+
